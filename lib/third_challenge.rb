508bd9427b303e0ff2f54c1d1fc3190ee3539a1c
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-08 02:06:33 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,20 +1,22 @@
 def third_challenge
   epic_tragedy = {
       :montague => {
+      
       :patriarch => {name: "Lord Montague", age: "53" },
       
       :matriarch => {name: "Lady Montague", age: "54" },
-      :hero => {name: "Romeo", age: "15", status: "alive"
-        
-      },
-      :hero_friends => []
-   }, 
-      :capulet => {name: "Lord Capulet" age: "50"},
-      :patriarch => {name: "Lady Capulet" age:"51"
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
+      
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      
+      :hero_friends => [] }, 
+      
+      :capulet =>
+      
+      :patriarch => {name: "Lady Capulet" age:"51" },
+     
+      :matriarch => {name: "Lady Capulet" age:"51" },
+       
+      
       :heroine => {name:"Juliet" age:"15" status:"alive"
         },
       :heroine_friends => []
