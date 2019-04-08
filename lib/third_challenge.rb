dd7cf2f6d6c7f779b1d997dfce5bf3d584d8c9fa
Ibrahim  Akar <ibsakar@gmail.com>
2019-04-08 01:57:56 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,9 +4,7 @@ def third_challenge
       :patriarch => {name: "Lord Montague", age: "53" },
       
        :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {
-        #your key/value pairs here
-        },
+      :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => []
    }, 
    :capulet => {
